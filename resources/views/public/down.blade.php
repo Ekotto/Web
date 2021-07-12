79fc2ebe4f6f9a57f0ff4e32b85090fc41e68459
Ekotto <ekottobryan1999@gmail.com>
2021-07-12 16:03:06 +0100

    12/07 changes

---
+++ resources/views/public/down.blade.php
@@ -0,0 +1,11 @@
+<h1>Select your download:</h1>
+
+<table class="table table-bordered">
+@foreach ($downloads as $download)
+    <tr>
+        <td>{{ $download->name }}</td>
+        <td>&pound;{{ round($download->price/100) }}</td>
+        <td><a href="/buy/{{ $download->id }}" class="btn btn-primary">Buy</a></td>
+    </tr>
+@endforeach
+</table>
